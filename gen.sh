5f83bf31d58133b9cea78563cbd3ebce071d8f2b
kiang <kiange@gmail.com>
2014-05-20 02:25:53 +0800

    change the c orders for commands and generate new outputs

--- gen.sh
+++ gen.sh
@@ -1,8 +1,11 @@
 # sudo apt-get install npm gdal-bin
 # sudo npm install -g mapshaper
-ogr2ogr -t_srs EPSG:4326 -s_srs EPSG:3826 -f "ESRI Shapefile" -lco ENCODING=UTF-8 3826.shp 全國村里界圖102.05.21_修正屬性表ID_TWD97/102_05_21台澎金馬修正村里屬性資料_TWD97.shp
-# ogr2ogr -t_srs EPSG:4326 -s_srs EPSG:3825 -f "ESRI Shapefile" -lco ENCODING=UTF-8 3825.shp 全國村里界圖102.05.21_修正屬性表ID_TWD97/102_05_21台澎金馬修正村里屬性資料_TWD97.shp
-mapshaper -p 0.01 3826.shp -f topojson --encoding utf8 -o 102_05_21_3826.json
-# mapshaper -p 0.01 3825.shp -f topojson --encoding utf8 -o 102_05_21_3825.json
-rm -f 3826.*
-# rm -f 3825.*
+
+# convert shp to geojson & big5 to utf8
+mapshaper -p 0.01 --auto-snap 全國村里界圖102.05.21_修正屬性表ID_TWD97/102_05_21台澎金馬修正村里屬性資料_TWD97.shp -f geojson --encoding big5 -o 102_05_21_twd97.geo.json
+
+# convert geojson polygons from EPSG:3826 to EPSG:4326
+ogr2ogr -t_srs EPSG:4326 -s_srs EPSG:3826 -f "GeoJSON" -lco ENCODING=UTF-8 102_05_21_3826.geo.json 102_05_21_twd97.geo.json
+
+# convert geojson to topojson
+mapshaper -p 0.01 --auto-snap 102_05_21_3826.geo.json -f topojson --encoding utf8 -o 102_05_21_3826.topo.json
