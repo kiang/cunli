1f628891a147140f40ab77e8b0098eac5d167fed
kiang <kiange@gmail.com>
2014-08-20 16:29:26 +0800

    update gen.sh to use new shp source

--- gen.sh
+++ gen.sh
@@ -2,10 +2,10 @@
 # sudo npm install -g mapshaper
 
 # convert shp to geojson & big5 to utf8
-mapshaper -p 0.01 --auto-snap -f geojson --encoding big5 -o json/102_05_21_twd97.geo.json 全國村里界圖102.05.21_修正屬性表ID_TWD97/102_05_21台澎金馬修正村里屬性資料_TWD97.shp
+mapshaper -p 0.01 --auto-snap -f geojson --encoding big5 -o json/103_05_01_twd97.geo.json shp/1030501/103_05_01台灣村里界\(含離島\)_TWD97_121.shp
 
 # convert geojson polygons from EPSG:3826 to EPSG:4326
-ogr2ogr -t_srs EPSG:4326 -s_srs EPSG:3826 -f "GeoJSON" -lco ENCODING=UTF-8 json/102_05_21_4326.geo.json json/102_05_21_twd97.geo.json
+ogr2ogr -t_srs EPSG:4326 -s_srs EPSG:3826 -f "GeoJSON" -lco ENCODING=UTF-8 json/103_05_01_4326.geo.json json/103_05_01_twd97.geo.json
 
 # convert geojson to topojson
-mapshaper -p 0.01 --auto-snap -f topojson --encoding utf8 -o json/102_05_21_4326.topo.json json/102_05_21_4326.geo.json
+mapshaper -p 0.01 --auto-snap -f topojson --encoding utf8 -o json/103_05_01_4326.topo.json json/103_05_01_4326.geo.json
